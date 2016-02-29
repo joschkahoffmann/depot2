a7afda70f3ea8552a835abbed0f077261ebec2fe
joschkahoffmann <joschkahoffmann@gmail.com>
2016-02-29 17:50:07 +1300

    cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,8 +6,20 @@ validates :image_url, allow_blank: true, format: {
 with: %r{\.(gif|jpg|png)\Z}i, 
 message: 'must be a URL for GIF, JPG or PNG image.'
 }
+has_many :line_items
+before_destroy :ensure_not_referenced_by_any_line_item
+	def ensure_not_referenced_by_an_line_item
+		if line_items.empty?
+			return true
+		else
+			errors.add(:base, 'Line Items present')
+			return false
+		end
+	end
+	
 
 	def self.latest
-		Product.order(updated_at).last 
+		Product.order(:updated_at).last 
 	end
+
 end
